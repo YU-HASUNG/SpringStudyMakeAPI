faa93b0196bfed8f56db76ddaca5df2ec4d55c1f
YU-HASUNG <harry.yu@kyobobook.com>
2025-11-21 19:21:16 +0900

    [201121] dummy api 생성

---
+++ src/main/java/com/helloworld/quickstart/controller/QuickStartController.java
@@ -0,0 +1,18 @@
+package com.helloworld.quickstart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuickStartController {
+
+	@GetMapping("/dummy")
+	public String dummy() {
+		return "{}";
+	}
+
+	@GetMapping("/dummy2")
+	public String dummy2() {
+		return "dummy2";
+	}
+}
