4150bff21ca5c7d15ba39740af50eb896b4768cc
YU-HASUNG <harry.yu@kyobobook.com>
2025-11-22 21:16:37 +0900

    [201121] lombok implement 설정

--- src/main/java/com/helloworld/quickstart/controller/QuickStartController.java
+++ src/main/java/com/helloworld/quickstart/controller/QuickStartController.java
@@ -3,16 +3,21 @@ package com.helloworld.quickstart.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class QuickStartController {
 
 	@GetMapping("/dummy")
 	public String dummy() {
+		log.info("dummy");
 		return "{}";
 	}
 
 	@GetMapping("/dummy2")
 	public String dummy2() {
+		log.info("dummy2");
 		return "dummy2";
 	}
 }
